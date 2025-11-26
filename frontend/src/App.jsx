a9f07564534a841f6c7be45557ebdc4a46ba7da3
Mikita Dzeviatau <9mikita@gmail.com>
2025-11-27 00:19:08 +0100

    feat: add Home, PageHeading, and PageTitle components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
 import React from "react";
+import Home from "./components/Home";
+import PageTitle from "./components/PageTitle";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Home />
       <Footer />
     </React.Fragment>
   );
---
+++ frontend/src/components/PageHeading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageTitle from "./PageTitle";
+
+function PageHeading() {
+  return (
+    <div className="page-heading-container">
+      <PageTitle title="Explore Those Cute Cats!" />
+    </div>
+  );
+}
+
+export default PageHeading;
