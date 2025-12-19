760c5fdecbd6bdfe292bba05c5ce5c869db06363
Mikita Dzeviatau <9mikita@gmail.com>
2025-12-19 23:39:13 +0100

    refactor: remove BootstrapButton component and related imports; clean up package dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
 import React from "react";
 import Home from "./components/Home";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import cats from "../data/cats";
 import PageHeading from "./PageHeading";
 import ProductListnigs from "./ProductListnigs";
-import BootstrapButton from "./BootstrapButton";
 import Snowfall from "react-snowfall";
 
 export default function Home() {
@@ -11,12 +10,6 @@ export default function Home() {
     <>
       <Snowfall color="#00ccff" />
       <div className="home-container">
-        <div className="container col-6">
-          <BootstrapButton text="Submit" type="primary" />
-          <BootstrapButton text="Save" type="secondary" />
-          <BootstrapButton text="Okay" type="success" />
-          <BootstrapButton text="Cancel" type="danger" />
-        </div>
         <PageHeading title="Explore Those Cute Cats!">
           Choose the cat and download the picture!
         </PageHeading>
