47281a0f12d8e06c59045e504bf741ca3a60e89b
Mikita Dzeviatau <9mikita@gmail.com>
2025-12-19 22:39:48 +0100

    feat: add BootstrapButton component and integrate react-snowfall in Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,22 @@
 import cats from "../data/cats";
 import PageHeading from "./PageHeading";
 import ProductListnigs from "./ProductListnigs";
-import EasyButton from "./EasyButton";
+import BootstrapButton from "./BootstrapButton";
+import Snowfall from "react-snowfall";
 
 export default function Home() {
   const isActive = Math.random() > 0.5;
 
   return (
     <>
+      <Snowfall color="#00ccff" />
       <div className="home-container">
+        <div className="container col-6">
+          <BootstrapButton text="Submit" type="primary" />
+          <BootstrapButton text="Save" type="secondary" />
+          <BootstrapButton text="Okay" type="success" />
+          <BootstrapButton text="Cancel" type="danger" />
+        </div>
         <PageHeading title="Explore Those Cute Cats!">
           Choose the cat and download the picture!
         </PageHeading>
