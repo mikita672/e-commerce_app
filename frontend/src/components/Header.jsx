42bc3da316d0257a95e4d7fd22ce0d4f78f9dd36
Mikita Dzeviatau <9mikita@gmail.com>
2025-12-26 21:32:16 +0100

    chore: rewrite header css with tailwind

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,37 +2,42 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingBasket, faCat } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const navLinkClass =
+    "text-center text-lg font-primary font-semibold text-primary py-2";
   return (
-    <header className="header">
-      <div className="container">
-        <a href="/" className="link">
-          <FontAwesomeIcon icon={faCat} className="fa-icon" />
-          <span className="brand-title">Meowterest</span>
+    <header className="border-b border-gray-300 sticky top-0 z-20 bg-purple-100">
+      <div className="flex items-center justify-between mx-auto max-w-6xl px-6 py-4">
+        <a
+          href="/"
+          className="text-center text-lg font-primary font-semibold text-primary py-2"
+        >
+          <FontAwesomeIcon icon={faCat} className="h-8 w-8" />
+          <span className="font-bold">Meowterest</span>
         </a>
-        <nav className="eazy-nav">
-          <ul>
+        <nav className="flex items-center py-2 z-10">
+          <ul className="flex space-x-6">
             <li>
-              <a href="/" className="navLink">
+              <a href="/" className={navLinkClass}>
                 Home
               </a>
             </li>
             <li>
-              <a href="/about" className="navLink">
+              <a href="/about" className={navLinkClass}>
                 About
               </a>
             </li>
             <li>
-              <a href="/contact" className="navLink">
+              <a href="/contact" className={navLinkClass}>
                 Contact
               </a>
             </li>
             <li>
-              <a href="/login" className="navLink">
+              <a href="/login" className={navLinkClass}>
                 Login
               </a>
             </li>
             <li>
-              <a href="/cart" className="navLink">
+              <a href="/cart" className={navLinkClass}>
                 <FontAwesomeIcon icon={faShoppingBasket} />
               </a>
             </li>
