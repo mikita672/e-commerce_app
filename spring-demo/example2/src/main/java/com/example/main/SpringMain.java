76c76150e9c324a300bedc36d41a6770bc68a7da
Mikita Dzeviatau <9mikita@gmail.com>
2025-12-27 23:58:43 +0100

    feat: add custom beans names demo

---
+++ spring-demo/example2/src/main/java/com/example/main/SpringMain.java
@@ -0,0 +1,23 @@
+package com.example.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+
+public class SpringMain {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle audiVehicle = context.getBean("audiVehicle", Vehicle.class);
+        System.out.println("Vehicle name: " + audiVehicle.getName());
+
+        Vehicle hondaVehicle = context.getBean("hondaVehicle", Vehicle.class);
+        System.out.println("Vehicle name: " + hondaVehicle.getName());
+
+        Vehicle ferrariVehicle = context.getBean("ferrariVehicle", Vehicle.class);
+        System.out.println("Vehicle name: " + ferrariVehicle.getName());
+
+        context.close();
+    }
+}
