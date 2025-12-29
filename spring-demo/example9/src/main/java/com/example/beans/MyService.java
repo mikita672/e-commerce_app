25deafbacf885eeebc109a6286e2e8f6e7d37f0b
Mikita Dzeviatau <9mikita@gmail.com>
2025-12-29 22:43:07 +0100

    feat: add lazy annotation demo

--- spring-demo/example9/src/main/java/com/example/beans/MyService.java
+++ spring-demo/example9/src/main/java/com/example/beans/MyService.java
@@ -1,10 +1,12 @@
 package com.example.beans;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("singleton")
+@Lazy
 public class MyService {
     public MyService() {
         System.out.println("My service created!");
--- spring-demo/example9/src/main/java/com/example/main/SpringMain.java
+++ spring-demo/example9/src/main/java/com/example/main/SpringMain.java
@@ -6,12 +6,11 @@ import com.example.beans.MyService;
 import com.example.config.ProjectConfig;
 
 public class SpringMain {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         MyService service1 = context.getBean(MyService.class);
         MyService service2 = context.getBean(MyService.class);
-        System.out.println(service1 == service2);
-
         context.close();
     }
 }
