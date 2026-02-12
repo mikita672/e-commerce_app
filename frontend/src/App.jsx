7226a27f70ca6b14343191f439f40af33c449faf
Mikita Dzeviatau <9mikita@gmail.com>
2026-02-12 19:25:04 +0100

    feat: implement routing with react-router-dom and add error handling page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
-import React from "react";
-import Home from "./components/Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
-    </React.Fragment>
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Login from "./components/Login.jsx";
+import Cart from "./components/Cart.jsx";
+import Home from "./components/Home.jsx";
+import ErrorPage from "./ErrorPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </StrictMode>,
-)
+);
