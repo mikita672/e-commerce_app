98a227e353629f091a8bee62707234913e6fa8cf
Mikita Dzeviatau <9mikita@gmail.com>
2026-02-25 01:04:45 +0100

    feat: implement CORS configuration and public path handling in security setup

---
+++ backend/src/main/java/com/mdzvtt/ecommerce/security/PublicPathConfig.java
@@ -0,0 +1,14 @@
+package com.mdzvtt.ecommerce.security;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PublicPathConfig {
+    @Bean
+    public List<String> publicPaths() {
+        return List.of("/api/v1/products/**", "/api/v1/contacts/**");
+    }
+}
--- backend/src/main/java/com/mdzvtt/ecommerce/security/SecurityConfig.java
+++ backend/src/main/java/com/mdzvtt/ecommerce/security/SecurityConfig.java
@@ -1,22 +1,52 @@
 package com.mdzvtt.ecommerce.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final List<String> publicPaths;
+
     @Bean
     SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                (requests) -> requests.requestMatchers("/api/v1/products/**").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        return http.csrf(csrfConfig -> csrfConfig.disable())
+                .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(
+                        (requests) -> {
+                            publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+                            requests.anyRequest().authenticated();
+                        })
+                .formLogin(withDefaults()).httpBasic(withDefaults()).build();
+    }
 
-        return http.build();
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
