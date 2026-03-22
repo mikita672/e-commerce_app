b8525a1bd7d5df262830d64b000d0df85c9b086f
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-22 02:02:58 +0100

    feat: add proper responses on login errors

--- backend/src/main/java/com/mdzvtt/ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/mdzvtt/ecommerce/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.mdzvtt.ecommerce.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,22 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody LoginRequestDto loginRequestDto) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password()));
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password()));
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(), null, null));
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(), null, null));
+        } catch (BadCredentialsException ex) {
+            return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Invalid username or password");
+        } catch (AuthenticationException ex) {
+            return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Authentication failed");
+        } catch (Exception ex) {
+            return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occured");
+        }
+    }
+
+    private ResponseEntity<LoginResponseDto> buildErrorResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status).body(new LoginResponseDto(message, null, null));
     }
 }
