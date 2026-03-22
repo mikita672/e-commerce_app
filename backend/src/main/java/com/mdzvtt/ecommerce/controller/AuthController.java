c5144d05373d9c9ac211e62bfe2b5485c809ca62
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-22 01:51:47 +0100

    feat: add authentication manager and updating AuthController for login

--- backend/src/main/java/com/mdzvtt/ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/mdzvtt/ecommerce/controller/AuthController.java
@@ -2,13 +2,15 @@ package com.mdzvtt.ecommerce.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mdzvtt.ecommerce.dto.LoginRequestDto;
 import com.mdzvtt.ecommerce.dto.LoginResponseDto;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,8 +18,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody LoginRequestDto loginRequestDto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password()));
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(), null, null));
     }
--- backend/src/main/java/com/mdzvtt/ecommerce/security/SecurityConfig.java
+++ backend/src/main/java/com/mdzvtt/ecommerce/security/SecurityConfig.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -38,6 +41,15 @@ public class SecurityConfig {
                             requests.anyRequest().authenticated();
                         })
                 .formLogin(withDefaults()).httpBasic(withDefaults()).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        var daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        var providerManager = new ProviderManager(daoAuthenticationProvider);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return providerManager;
     }
 
     @Bean
